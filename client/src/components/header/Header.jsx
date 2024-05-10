1134e444cc21a3bf8f4707243357123b23af856b
simo blues <wambuisimon712@gmail.com>
2024-05-10 06:28:24 +0300

    changes

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import './header.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBed } from '@fortawesome/free-solid-svg-icons'
+import { faBed, faCar, faPlane, faTaxi } from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => {
   return (
@@ -8,8 +8,24 @@ const Header = () => {
         <div className="header">
           <div className="headerList">
             <div className="headerListItem">
-            <FontAwesomeIcon  icon={faBed} />
-            <span>Stays</span>
+             <FontAwesomeIcon  icon={faBed} />
+             <span>Stays</span>
+            </div>
+            <div className="headerListItem">
+             <FontAwesomeIcon  icon={faPlane} />
+             <span>Flights</span>
+            </div>
+            <div className="headerListItem">
+             <FontAwesomeIcon  icon={faCar} />
+             <span>Car rentals</span>
+            </div>
+            <div className="headerListItem">
+             <FontAwesomeIcon  icon={faBed} />
+             <span>Attractions</span>
+            </div>
+            <div className="headerListItem">
+             <FontAwesomeIcon  icon={faTaxi} />
+             <span>Airport taxis</span>
             </div>
           </div>
         </div>
