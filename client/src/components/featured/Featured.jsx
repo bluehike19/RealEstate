41197a0d1a2cd9f439e0117fad6c3c9d3798446e
simo blues <wambuisimon712@gmail.com>
2024-05-15 22:42:18 +0300

    chages

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -4,28 +4,28 @@ const Featured = () => {
   return (
     <div className='featured'>
         <div className="featuredItem">
-            <img src="/pic1.jpg"  className="featuredImg" />
+            <img src="pic1.jpg"  className="featuredImg" />
             <div className="featuredTitles">
                 <h1>Dublin</h1>
                 <h2>123 properties</h2>
             </div>
         </div>
         <div className="featuredItem">
-            <img src=""  className="featuredImg" />
+            <img src="pic2.jpg"  className="featuredImg" />
             <div className="featuredTitles">
                 <h1>Dublin</h1>
                 <h2>123 properties</h2>
             </div>
         </div>
         <div className="featuredItem">
-            <img src=""  className="featuredImg" />
+            <img src="pic3.jpg"  className="featuredImg" />
             <div className="featuredTitles">
                 <h1>Austin</h1>
                 <h2>532 properties</h2>
             </div>
         </div>
         <div className="featuredItem">
-            <img src=""  className="featuredImg" />
+            <img src="pic4.jpg"  className="featuredImg" />
             <div className="featuredTitles">
                 <h1>Reno</h1>
                 <h2>533 properties</h2>
