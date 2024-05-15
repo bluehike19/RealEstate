d32d12a2257d759f577d3241a93366bea0726e33
simo blues <wambuisimon712@gmail.com>
2024-05-15 22:32:37 +0300

    changes

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -4,7 +4,7 @@ const Featured = () => {
   return (
     <div className='featured'>
         <div className="featuredItem">
-            <img src=""  className="featuredImg" />
+            <img src="/pic1.jpg"  className="featuredImg" />
             <div className="featuredTitles">
                 <h1>Dublin</h1>
                 <h2>123 properties</h2>
