144422eee20375a5a0213c7e92a0283969961436
simo blues <wambuisimon712@gmail.com>
2024-05-17 05:47:46 +0300

    changes

--- client/src/components/propertyList/propertyList.jsx
+++ client/src/components/propertyList/propertyList.jsx
@@ -31,13 +31,7 @@ const propertyList = () => {
           <h1>233 hotels</h1>
         </div>
       </div>
-      <div className="pListItem">
-        <img src="pic4.jpg" className="pListImg" />
-        <div className="pListTitles">
-          <h1>Hotels</h1>
-          <h1>233 hotels</h1>
-        </div>
-      </div>
+     
     </div>
   )
 }
