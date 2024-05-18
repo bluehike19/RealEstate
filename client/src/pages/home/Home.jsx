9a1788e8de68f798aaecd36019b71720315debbf
simo blues <wambuisimon712@gmail.com>
2024-05-18 22:34:03 +0300

    changes

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,7 +1,6 @@
 import Featured from '../../components/featured/Featured'
 import Header from '../../components/header/Header'
 import Navbar from '../../components/navbar/Navbar'
-import PropertyList from '../../components/propertyList/PropertyList'
 import './home.css'
 
 const Home = () => {
