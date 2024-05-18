732154ea791e2f36f7b321225c3c2f95a994a535
simo blues <wambuisimon712@gmail.com>
2024-05-18 22:42:47 +0300

    changes

---
+++ client/src/components/featuredProperty/FeaturedProperty.jsx
@@ -0,0 +1,10 @@
+import './featuredProperty.css'
+import React from 'react'
+
+const FeaturedProperty = () => {
+  return (
+    <div>FeaturedProperty</div>
+  )
+}
+
+export default FeaturedProperty
