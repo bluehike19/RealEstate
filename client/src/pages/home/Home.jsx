bd7868d78baf55c66dd879190ba8f0c81af0482c
simo blues <wambuisimon712@gmail.com>
2024-05-19 05:55:59 +0300

    changes

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import Featured from '../../components/featured/Featured'
 import Header from '../../components/header/Header'
 import Navbar from '../../components/navbar/Navbar'
-import PropertyList from '../../components/propertyList/PropertList'
+import PropertyList from '../../components/propertyList/PropertyList'
 import './home.css'
 
 const Home = () => {
@@ -11,7 +11,7 @@ const Home = () => {
       <Header />
       <div className="homeContainer">
         <Featured/>
-        <h1 className="homeTitle">Browse by property type</h1>
+        <div className="homeTitle">Browse by property type</div>
         <PropertyList />
         <h1 className="homeTitle">Homes guests love</h1>
       </div>
