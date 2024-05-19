33c37e4a2f93a96de7aae73a3357b07aa12f6a5c
simo blues <wambuisimon712@gmail.com>
2024-05-19 06:02:40 +0300

    changes

--- client/src/components/featuredProperty/FeaturedProperty.jsx
+++ client/src/components/featuredProperty/FeaturedProperty.jsx
@@ -3,7 +3,16 @@ import React from 'react'
 
 const FeaturedProperty = () => {
   return (
-    <div>FeaturedProperty</div>
+    <div className='fp'>
+      <img src="pic1.jpg" alt="" />
+      <span className="fpName">Aparthotel Stare Miasto</span>
+      <span className="fpCity">Madrid</span>
+      <span className="fpPrice">Starting from $120</span>
+      <div className="fpRating">
+        <button>8.9</button>
+        <span>Excellent</span>
+      </div>
+    </div>
   )
 }
 
