553caed2b345286a581126c83d3fc45a573875e4
simo blues <wambuisimon712@gmail.com>
2024-05-19 06:07:14 +0300

    changes

--- client/src/components/featuredProperty/FeaturedProperty.jsx
+++ client/src/components/featuredProperty/FeaturedProperty.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 
 const FeaturedProperty = () => {
   return (
+    <>
     <div className='fp'>
       <img src="pic1.jpg" alt="" />
       <span className="fpName">Aparthotel Stare Miasto</span>
@@ -13,6 +14,27 @@ const FeaturedProperty = () => {
         <span>Excellent</span>
       </div>
     </div>
+    <div className='fp'>
+    <img src="pic1.jpg" alt="" />
+    <span className="fpName">Aparthotel Stare Miasto</span>
+    <span className="fpCity">Madrid</span>
+    <span className="fpPrice">Starting from $120</span>
+    <div className="fpRating">
+      <button>8.9</button>
+      <span>Excellent</span>
+    </div>
+  </div>
+  <div className='fp'>
+  <img src="pic1.jpg" alt="" />
+  <span className="fpName">Aparthotel Stare Miasto</span>
+  <span className="fpCity">Madrid</span>
+  <span className="fpPrice">Starting from $120</span>
+  <div className="fpRating">
+    <button>8.9</button>
+    <span>Excellent</span>
+  </div>
+</div>
+</>
   )
 }
 
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,4 +1,5 @@
 import Featured from '../../components/featured/Featured'
+import FeaturedProperty from '../../components/featuredProperty/FeaturedProperty'
 import Header from '../../components/header/Header'
 import Navbar from '../../components/navbar/Navbar'
 import PropertyList from '../../components/propertyList/PropertyList'
@@ -14,6 +15,7 @@ const Home = () => {
         <div className="homeTitle">Browse by property type</div>
         <PropertyList />
         <h1 className="homeTitle">Homes guests love</h1>
+        <FeaturedProperty/>
       </div>
     </div>
   )
