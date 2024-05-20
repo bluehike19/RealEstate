ff9b6c11e76b6dd25367ccd728c8d263d1480fad
simo blues <wambuisimon712@gmail.com>
2024-05-20 06:03:07 +0300

    changes

--- client/src/components/mailList/MailList.jsx
+++ client/src/components/mailList/MailList.jsx
@@ -5,6 +5,11 @@ const MailList = () => {
   return (
     <div className="mail">
         <h1 className="mailTitle"></h1>
+        <span className="mailDesc"></span>
+        <div className="mailInputContainer">
+            <input type="text" placeholder='Your Email' />
+            <button>Subscribe</button>
+        </div>
     </div>
   )
 }
