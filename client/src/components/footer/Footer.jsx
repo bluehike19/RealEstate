6fe35972db5a45920a70fdcf1c16e868e279182f
simo blues <wambuisimon712@gmail.com>
2024-05-21 06:33:07 +0300

    changes

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -5,7 +5,52 @@ const Footer = () => {
     <div className='footer'>
         <div className="fLists">
             <ul className="fList">
-                <li className="fListItem"></li>
+                <li className="fListItem">Countries</li>
+                <li className="fListItem">Region</li>
+                <li className="fListItem">Cities</li>
+                <li className="fListItem">Districts</li>
+                <li className="fListItem">Airports</li>
+                <li className="fListItem">Hotels</li>
+            </ul>
+            <ul className="fList">
+                <li className="fListItem">Countries</li>
+                <li className="fListItem">Region</li>
+                <li className="fListItem">Cities</li>
+                <li className="fListItem">Districts</li>
+                <li className="fListItem">Airports</li>
+                <li className="fListItem">Hotels</li>
+            </ul>
+            <ul className="fList">
+                <li className="fListItem">Countries</li>
+                <li className="fListItem">Region</li>
+                <li className="fListItem">Cities</li>
+                <li className="fListItem">Districts</li>
+                <li className="fListItem">Airports</li>
+                <li className="fListItem">Hotels</li>
+            </ul>
+            <ul className="fList">
+                <li className="fListItem">Countries</li>
+                <li className="fListItem">Region</li>
+                <li className="fListItem">Cities</li>
+                <li className="fListItem">Districts</li>
+                <li className="fListItem">Airports</li>
+                <li className="fListItem">Hotels</li>
+            </ul>
+            <ul className="fList">
+                <li className="fListItem">Countries</li>
+                <li className="fListItem">Region</li>
+                <li className="fListItem">Cities</li>
+                <li className="fListItem">Districts</li>
+                <li className="fListItem">Airports</li>
+                <li className="fListItem">Hotels</li>
+            </ul>
+            <ul className="fList">
+                <li className="fListItem">Countries</li>
+                <li className="fListItem">Region</li>
+                <li className="fListItem">Cities</li>
+                <li className="fListItem">Districts</li>
+                <li className="fListItem">Airports</li>
+                <li className="fListItem">Hotels</li>
             </ul>
         </div>
     </div>
