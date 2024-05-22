c9c0f15e566acf7890e436f5c6323e4220f0e322
simo blues <wambuisimon712@gmail.com>
2024-05-22 18:04:37 +0300

    changes

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -3,11 +3,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBed, faCalendarDays, faCar, faPerson, faPlane, faTaxi } from '@fortawesome/free-solid-svg-icons'
 import { DateRange } from 'react-date-range'
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 import {format} from 'date-fns'
 
 const Header = ({type}) => {
+  const [destination, setDestination] = useState("")
   const [openDate, setOpenDate] = useState(false)
   const [date, setDate] = useState([
     {
@@ -23,6 +25,8 @@ const Header = ({type}) => {
     room: 1,
   })
 
+  const navigate = useNavigate()
+
   const handleOption = (name, operation) => {
     setOptions((prev)=> {
       return {
@@ -30,6 +34,10 @@ const Header = ({type}) => {
         [name]: operation === "i" ? options[name] + 1: options[name] - 1,
       }
     })
+  }
+
+  const handleSearch = () => {
+    navigate("/hotels", {state: { destination, date, options}})
   }
 
   return (
@@ -66,7 +74,7 @@ const Header = ({type}) => {
           <div className="headerSearch">
             <div className="headerSearchItem">
               <FontAwesomeIcon icon={faBed} className='headerIcon' />
-              <input type="text" placeholder='Where are you going?' className='headerSearchInput' />
+              <input type="text" placeholder='Where are you going?' className='headerSearchInput' onChange={e=>setDestination(e.target.value)} />
             </div>
             <div className="headerSearchItem">
               <FontAwesomeIcon icon={faCalendarDays} className='headerIcon' />
@@ -104,7 +112,7 @@ const Header = ({type}) => {
               </div>}
             </div>
             <div className="headerSearchItem">
-              <button className="headerBtn">Search</button>
+              <button className="headerBtn" onClick={handleSearch}>Search</button>
             </div>
           </div></>}
         </div>
--- client/src/pages/list/List.jsx
+++ client/src/pages/list/List.jsx
@@ -1,8 +1,14 @@
+import { useLocation } from 'react-router-dom'
 import Header from '../../components/header/Header'
 import Navbar from '../../components/navbar/Navbar'
 import './list.css'
 
 const List = () => {
+
+  const location = useLocation()
+
+  console.log(location);
+
   return (
     <div>
       <Navbar/>
