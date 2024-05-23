d4574ee6db692e8e1cc7a53fffc9f7ca2eb6a00f
simo blues <wambuisimon712@gmail.com>
2024-05-23 06:45:54 +0300

    changes

--- client/src/components/searchItem/SearchItem.jsx
+++ client/src/components/searchItem/SearchItem.jsx
@@ -10,7 +10,7 @@ const SearchItem = () => {
             <span className="siTaxiOp">Free airport taxi</span>
             <span className="siSubtitle">Studio Apartment with air conditioning</span>
             <span className="siFeature">Entire studio . 1 bathroom 21m2 1 full bed </span>
-            <span className="siCanceLop">Free cancellation</span>
+            <span className="siCancelOp">Free cancellation</span>
             <span className="siCancelOpSubtitle">You can cancel later, so lock in this great price today!</span>
         </div>    
          <div className="siDetails">details</div>
