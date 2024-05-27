811622d363bb13b4377fc65aedda2e0dfc1e8920
simo blues <wambuisimon712@gmail.com>
2024-05-27 17:47:33 +0300

    changes

--- client/src/pages/hotel/Hotel.jsx
+++ client/src/pages/hotel/Hotel.jsx
@@ -1,13 +1,27 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Header from '../../components/header/Header';
 import Navbar from '../../components/navbar/Navbar';
 import './hotel.css'
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 
 const Hotel = () => {
   return (
     <div>
     <Navbar/>
     <Header type='list'/>
-    span
+    <div className="hotelContainer">
+      <div className="hotelWrapper">
+        <h1 className="hotelTitle">Grand Hotel</h1>
+        <div className="hotelAddress">
+          <FontAwesomeIcon icon={faLocationDot} />
+          <span>Elton St 125 New York</span>
+        </div>
+        <span className="hotelDistance">Excellent location - 500m from center</span>
+        <span className="hotelPriceHighlight">
+          Book a stay over $114 at this property and get a free airport taxi
+        </span>
+      </div>
+    </div>
     </div>
   )
 }
