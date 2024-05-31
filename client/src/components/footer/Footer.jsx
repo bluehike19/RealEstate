42c25e4dc603a6da199f0d2f8c089c2e96c81482
simo blues <wambuisimon712@gmail.com>
2024-05-31 22:23:33 +0300

    changes

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -44,6 +44,7 @@ const Footer = () => {
                 <li className="fListItem">Airports</li>
                 <li className="fListItem">Hotels</li>
             </ul>
+            
             <ul className="fList">
                 <li className="fListItem">Countries</li>
                 <li className="fListItem">Region</li>
