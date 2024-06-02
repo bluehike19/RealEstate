99c9234d64b3e622a9f1da16d855bfc11eccdef4
simo blues <wambuisimon712@gmail.com>
2024-06-02 23:33:14 +0300

    changes

--- api/models/Hotel.js
+++ api/models/Hotel.js
@@ -5,6 +5,14 @@ const HotelSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    type: {
+        type: String,
+        required: true,
+    },
+    city: {
+        type: String,
+        required: true,
+    },
 })
 
 export default mongoose.model("Hotel", HotelSchema)
