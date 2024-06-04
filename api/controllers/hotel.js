da09a3b9c2fe3dfd0421f381afa39a910718315f
simo blues <wambuisimon712@gmail.com>
2024-06-04 22:26:34 +0300

    changes

--- api/controllers/hotel.js
+++ api/controllers/hotel.js
@@ -39,8 +39,21 @@ export const getHotel = async (req, res, next) => {
 
     try {
         const hotel = await Hotel.findById(req.params.id);
-        res.status(200).json(hotel);
+        res.status(200).json(hotel); 
     } catch (err) {
         next(err)
     }
+};
+
+export const getHotels = async (req, res, next) => {
+    const {min, max, ...others} = req.query; 
+    try {
+        const hotels = await Hotel.find({
+            ...others,
+            cheapestPrice: { $gt: min | 1, $lt: max || 999},
+        }).limit(req.query.limit);
+        res.status(200).json(hotels)
+     } catch (err) {
+        next(err)
+     }
 }
