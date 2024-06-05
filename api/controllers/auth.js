1a13c84e42962eed3b793b62d60be618ad18dcf7
simo blues <wambuisimon712@gmail.com>
2024-06-05 22:59:48 +0300

    changes

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -5,4 +5,10 @@ import { createError } from "../utils/error"
 export const createRoom = async (req, res, next) => { 
     const hotelId = req.params.hotelId;
     const newRoom = new Room(req.body)
+
+    try {
+        const 
+    } catch (err) {
+        next(err)
+    }
 }
