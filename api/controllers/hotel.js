87ba3d68e2d17ebd4e7eb9453c82c8b75e4c5105
simo blues <wambuisimon712@gmail.com>
2024-06-05 22:18:59 +0300

    changes

--- api/controllers/hotel.js
+++ api/controllers/hotel.js
@@ -91,3 +91,17 @@ export const countyByType = async (req, res, next) => {
         next(err)
     }
 }
+
+export const getHotelRooms = async (req, res, next) => {
+    try {
+        const hotel = await Hotel.findById(req.params.id);
+        const list = await Promise.all(
+            hotel.rooms.map((room)=> {
+                return room.findById(room);
+            })
+        );
+        res.status(200).json(list)
+    } catch (err) {
+        next(err)
+    }
+}
