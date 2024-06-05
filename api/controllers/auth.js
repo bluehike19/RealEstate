c6ae4b85ffe9234bc30cd67290aadaaefefc2e42
simo blues <wambuisimon712@gmail.com>
2024-06-05 22:56:25 +0300

    changes

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,4 +1,7 @@
 import Room from "../models/Room";
 import Hotel from "../models/Hotel";
-
 import { createError } from "../utils/error"
+
+export const createRoom = async (req, res, next) => { 
+    
+}
