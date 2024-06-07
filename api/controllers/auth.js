11d36e22461915a1ffa4916cc6e556b69859fd7c
simo blues <wambuisimon712@gmail.com>
2024-06-07 23:01:34 +0300

    changes

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -60,3 +60,12 @@ export const getRoom = async (req, res, next) => {
         next(err)
     }
 }
+
+export const getRooms = async (req, res, next) => {
+    try {
+        const rooms = await Room.find();
+        res.status(200).json(rooms)
+    } catch (err) {
+        next(err)
+    }
+}
