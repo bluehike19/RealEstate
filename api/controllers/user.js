38ff683fad2a57c18afe3f5662dedbf4b49006f5
simo blues <wambuisimon712@gmail.com>
2024-06-09 23:55:55 +0300

    changes

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -15,7 +15,17 @@ export const updateUser = async (req, res, next) => {
 
 export const deleteUser = async (req, res, next) => {
     try {
-        
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("User has been deleted.")
+    } catch (err) {
+        next(err)
+    }
+}
+
+export const getUser = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.params.id);
+        res.status(200).json(user)
     } catch (err) {
         next(err)
     }
