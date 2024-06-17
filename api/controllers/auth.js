787177a8624e58cf9cef5dac8f46181e4f47d46c
simo blues <wambuisimon712@gmail.com>
2024-06-17 22:02:01 +0300

    changes

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -3,3 +3,10 @@ import bcrypt from "bcryptjs"
 import { createError } from "../utils/error.js"
 import jwt from "jasonwebtoken"
 
+export const register = async (req, res, next) => {
+    try {
+        
+    } catch (err) {
+        next(err)
+    }
+}
