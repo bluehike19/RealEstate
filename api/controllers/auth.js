54bf1be866a3f77ba182499acb3a3639e878009b
simo blues <wambuisimon712@gmail.com>
2024-06-18 22:58:03 +0300

    changes

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -22,7 +22,7 @@ export const register = async (req, res, next) => {
 
 export const login = async (req, res, next) => {
     try {
-        const user = User.findOne({username:req.body.username})
+        const user = await User.findOne({username:req.body.username})
         if(!user) return next(createError(404, "User not found!"));
 
         const isPasswordCorrect = await bcrypt.compare(
@@ -31,8 +31,20 @@ export const login = async (req, res, next) => {
         );
         if(!isPasswordCorrect)
             return next(createError(400, "Wrong password or username!"));
-        
-        res.status(200).send("User has been created.")
+
+        const token = jwt.sign(
+            { id: user._id, isAdmin: user.isAdmin },
+            process.env.JWT
+        );
+
+        const { password, isAdmin, ...otherDetails } = user._doc;
+        res
+           .cookie("access_token", token, {
+            httpOnly: true,
+           })
+           .status(200)
+           .json({ details: { ...otherDetails }, isAdmin})
+
     } catch (err) {
         next(err)
     }
