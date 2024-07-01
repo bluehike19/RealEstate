24f5e7a339c2530b01911f244d4eee1a987fdaee
simo blues <wambuisimon712@gmail.com>
2024-07-01 20:57:55 +0300

    changes

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import { updateUser, createUser, deleteUser, getUser, getUsers } from '../controllers/user';
+import { verifyToken } from '../utils/verifyToken';
 
 const router = express.Router()
 
 //CREATE
 router.post("/", createUser)
+
+router.get("/checkAuthentication", verifyToken, (req,res,next)=> {
+    res.send("hello user, you are logged in and you can delete your account")
+})
 
 //UPDATE
 router.put("/:id", updateUser)
